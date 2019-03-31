616d19faa110e046786ff923db951498fda3bdc6
alsvid <alsvid.git@gmail.com>
2019-03-31 20:31:02 +0200

    added rotations

--- assets/js/renderer.js
+++ assets/js/renderer.js
@@ -15,10 +15,11 @@ function draw() {
     rectMode(CENTER);
     translate(mouseX - width/2, mouseY - height/2);
     rotateX(angle);
+    rotateY(angle);
+    rotateZ(angle);
     fill(50);
     rotate
     texture(icing);
     torus(100, 50);
     angle += 0.035;
-    console.log(angle)
 }
